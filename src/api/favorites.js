c31d66b7f0712b1173faa6ae792cccf4ed07bff4
Lilli Reilly <lilli.reilly@gmail.com>
2022-03-01 21:08:39 +0100

    Deleted favorite structure

--- src/api/favorites.js
+++
@@ -1,8 +0,0 @@
-import { api } from "./api"
-
-export function addFavorite(favorite) {
-    return api.post("/favorites", favorite)
-}
-export function deleteFavorite(favoriteId, favorite) {
-    return api.delete(`/favorites/${favoriteId}`, favorite)
-}

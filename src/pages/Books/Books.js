718edde2c123c9eaf0cb70eb20a2eda3e2dfe5f0
Lilli Reilly <lilli.reilly@gmail.com>
2022-03-03 21:24:19 +0100

    Search and favorite work

--- src/pages/Books/Books.js
+++ src/pages/Books/Books.js
@@ -3,25 +3,37 @@ import { getBooks } from "../../api/books";
 import { UseFetch } from "../../hooks/useFetch";
 import { Suspense } from "../../components/Suspense";
 import { Link } from "react-router-dom";
+//import { useState } from "react";
 
 
-function books() {
+function Books() {
   const { data, loading, error } = UseFetch(getBooks);
-  
+  const [searchWord, setSearchWord] = React.useState("")
+  const handleSearch = ({ target }) => {
+    setSearchWord(target.value)
+  }
+  const searchedWord = (book) =>
+  book.title.toLowerCase().includes(searchWord);
   return (
     <Suspense noData={!data && !loading} error={error} loading={loading}>
-      <h2>Books</h2>
       <div>
-        {data?.map((book) => (
-          <div key={book._id}>
-            <p>{book.title}</p>
-            <img src={book.imageUrl} alt="cover" className="books-cover"/>
-            <Link to={`/books/${book._id}`}>show more</Link>
-          </div>
-        ))}
+      <h2>Books</h2>
+        <input className="search" type="text" name="search" id="search" placeholder="search by title"
+        value={searchWord} onChange={handleSearch} />
+      <div className="Search-books">
+      {data?.filter(searchedWord).map((book) => (
+        <div key={book._id}>
+        <img src={book.imageUrl} alt="cover" className="books-search"/>
+        <a href={`/books/${book._id}`}>{book.title}</a>
+        </div>
+      ))}
+      </div>
+      </div>
+      <div>
+
       </div>
     </Suspense>
   );
 }
 
-export default books;
+export default Books;
